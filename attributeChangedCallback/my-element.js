fa021b70c1c18d95118946363d1bb8098908cb8c
Antonio <antonio@figueroamx.com>
2025-01-08 14:30:05 -0600

    AtributeChangedCallback

---
+++ attributeChangedCallback/my-element.js
@@ -0,0 +1,51 @@
+class myElement extends HTMLElement{
+    constructor(){
+        super();
+        this.attachShadow({mode: "open"});
+
+    }
+    static get observedAttributes(){
+        return ["title","parrafo","img"];
+    }
+attributeChangedCallback(attr, oldVal, newVal) {
+        if (oldVal !== newVal) {
+            this[attr] = newVal
+        }
+        this.render();
+    }
+    getTemplate(){
+        const template = document.createElement("template");
+        template.innerHTML =  `
+            <section>
+                <h2>${this.title}</h2>
+                <div>
+                    <p>${this.parrafo}</p>
+                    <input type="text">
+                    <img src="${this.img}">
+                </div>
+            </section>
+            ${this.getStyles()}
+        `;
+        return template;
+    }
+    getStyles(){
+        return `
+            <style>
+                h2{
+                    color: red;
+                }
+                p{
+                    color: blue;
+                }
+            </style>
+        `;
+    };
+    render(){
+        this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
+    }
+    connectedCallback(){
+        this.innerHTML = ``;
+        this.render();
+    }
+}
+customElements.define("my-element",myElement)
