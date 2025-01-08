e5f98a4588c9ce1ad71fe8ffc1d32e9bc46e7608
Antonio <antonio@figueroamx.com>
2025-01-07 19:36:43 -0600

    Fin primera clase

--- CustomElements/my-element.js
+++ CustomElements/my-element.js
@@ -1,7 +1,26 @@
+const template = document.createElement("div");
+template.innerHTML =  `
+    <style>
+        .texto{
+            color: red;
+        }
+        p{
+            color: blue;
+        }
+    </style>
+    <p class="texto"> Hello World  2</p>
+    <p> Texto ejemplo</p>
+`
 class myElement extends HTMLElement{
     constructor(){
         super();
-        console.log("Hola mundo")
+
+        this.p = document.createElement("p");
+    }
+    connectedCallback(){
+        this.p.textContent = "Hello World";
+        this.appendChild(this.p);
+        this.appendChild(template);
     }
 }
 customElements.define("my-element",myElement)
